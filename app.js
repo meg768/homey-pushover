2d2e51308d92495567dcf06def7f709ff6203aac
meg768 <meg768@users.noreply.github.com>
2023-10-28 00:00:33 +0200

    -

--- app.js
+++ app.js
@@ -9,12 +9,50 @@ class MyApp extends Homey.App {
 	async onInit() {
 		this.log('MyApp has been initialized');
 
+		this.addAction('pushover-notification-priority-high', async (args) => {
+			const PushoverNotifications = require('pushover-notifications');
+			const { message } = args;
+			let priority = '1';
+
+			if (!message) {
+				throw new Error(`Need to specify a message.`);
+			}
+
+			if (!priority) {
+				priority = '0';
+			}
+
+			if (message == '') {
+				message = '{Empty message}';
+			}
+
+			let user = Homey.env.user;
+			let token = Homey.env.token;
+			let payload = { priority: parseInt(priority), message: message };
+
+			if (typeof user != 'string' || typeof token != 'string') {
+				throw new Error(`Need to specify Pushover user and token in settings.`);
+			}
+
+			this.log(`Sending payload ${JSON.stringify(payload)} to Pushover.`);
+
+			var push = new PushoverNotifications({ user: user, token: token });
+
+			// See https://pushover.net/api for payload parameters
+
+			push.send(payload, function (error, result) {
+				if (error) {
+					this.log(error.stack);
+				}
+			});
+		});
+
 		this.addAction('pushover-notification', async (args) => {
 			const PushoverNotifications = require('pushover-notifications');
 			const { message } = args;
-            let priority = '0';
-			
-            if (!message) {
+			let priority = '0';
+
+			if (!message) {
 				throw new Error(`Need to specify a message.`);
 			}
 
