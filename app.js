bca73451110c2170cf35d5ce6843d62c67169fe9
meg768 <meg768@users.noreply.github.com>
2023-10-28 00:55:34 +0200

    -

--- app.js
+++ app.js
@@ -3,86 +3,59 @@
 const Homey = require('homey');
 
 class MyApp extends Homey.App {
-	/**
-	 * onInit is called when the app is initialized.
-	 */
+	async pushover({ message, priority, token, user }) {
+		const PushoverNotifications = require('pushover-notifications');
+
+		if (priority == undefined) {
+			priority = 0;
+		}
+
+		if (typeof priority == 'string') {
+			priority = parseInt(priority);
+		}
+
+		if (typeof message != 'string' || message == '') {
+			message = '{Empty message}';
+		}
+
+		if (user == undefined) {
+			user = Homey.env.user;
+		}
+
+		if (token == undefined) {
+			token = Homey.env.token;
+		}
+
+		if (typeof user != 'string' || typeof token != 'string') {
+			throw new Error(`Need to specify Pushover user and token in settings.`);
+		}
+
+		let payload = { priority: priority, message: message };
+
+		this.log(`Sending payload ${JSON.stringify(payload)} to Pushover.`);
+
+		var push = new PushoverNotifications({ user: user, token: token });
+
+		// See https://pushover.net/api for payload parameters
+
+		push.send(payload, function (error, result) {
+			if (error) {
+				this.log(error.stack);
+			}
+		});
+	}
+
 	async onInit() {
 		this.log('MyApp has been initialized');
 
 		this.addAction('pushover-notification-priority-high', async (args) => {
-			const PushoverNotifications = require('pushover-notifications');
-			const { message } = args;
-			let priority = '1';
-
-			if (!message) {
-				throw new Error(`Need to specify a message.`);
-			}
-
-			if (!priority) {
-				priority = '0';
-			}
-
-			if (message == '') {
-				message = '{Empty message}';
-			}
-
-			let user = Homey.env.user;
-			let token = Homey.env.token;
-			let payload = { priority: parseInt(priority), message: message };
-
-			if (typeof user != 'string' || typeof token != 'string') {
-				throw new Error(`Need to specify Pushover user and token in settings.`);
-			}
-
-			this.log(`Sending payload ${JSON.stringify(payload)} to Pushover.`);
-
-			var push = new PushoverNotifications({ user: user, token: token });
-
-			// See https://pushover.net/api for payload parameters
-
-			push.send(payload, function (error, result) {
-				if (error) {
-					this.log(error.stack);
-				}
-			});
+			let { message, priority = 1 } = args;
+			this.pushover({ message: message, priority: priority });
 		});
 
 		this.addAction('pushover-notification', async (args) => {
-			const PushoverNotifications = require('pushover-notifications');
-			const { message } = args;
-			let priority = '0';
-
-			if (!message) {
-				throw new Error(`Need to specify a message.`);
-			}
-
-			if (!priority) {
-				priority = '0';
-			}
-
-			if (message == '') {
-				message = '{Empty message}';
-			}
-
-			let user = Homey.env.user;
-			let token = Homey.env.token;
-			let payload = { priority: parseInt(priority), message: message };
-
-			if (typeof user != 'string' || typeof token != 'string') {
-				throw new Error(`Need to specify Pushover user and token in settings.`);
-			}
-
-			this.log(`Sending payload ${JSON.stringify(payload)} to Pushover.`);
-
-			var push = new PushoverNotifications({ user: user, token: token });
-
-			// See https://pushover.net/api for payload parameters
-
-			push.send(payload, function (error, result) {
-				if (error) {
-					this.log(error.stack);
-				}
-			});
+			let { message, priority = 0 } = args;
+			this.pushover({ message: message, priority: priority });
 		});
 	}
 
